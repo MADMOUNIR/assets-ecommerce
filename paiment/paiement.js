653f0ddf77e935298bfe74a19a487d1876b00957
Espero AKPOLI <contact@espero-soft.com>
2020-11-19 22:01:25 +0100

    update paiement

--- paiment/paiement.js
+++ paiment/paiement.js
@@ -1,55 +1,60 @@
-{
-    currency: '',
-    clientId: '',
-    // tslint:disable-next-line: no-angle-bracket-type-assertion
-    createOrderOnClient: (data) => < ICreateOrderRequest > {
-        intent: 'CAPTURE',
-        purchase_units: [{
-            amount: {
-                currency_code: '',
-                value: 20.0,
-                breakdown: {
-                    item_total: {
-                        currency_code: '',
-                        value: 20.0
-                    },
-                }
-            },
-            items: [{
-                name: 'PAIEMENT JSTORE SHOP',
-                quantity: '1',
-                category: 'SHOPPING FEMME',
-                unit_amount: {
-                    currency_code: '',
-                    value: 20.0,
-                },
-            }]
-        }]
-    },
-    advanced: {
-        commit: 'true'
-    },
-    style: {
-        label: 'paypal',
-        layout: 'vertical'
-    },
-    onApprove: (data, actions) => {
-        console.log('onApprove - transaction was approved, but not authorized', data, actions);
-        actions.order.get().then(details => {
-            console.log('onApprove - you can get full order details inside onApprove: ', details);
-        });
-    },
-    onClientAuthorization: (data) => {
-        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+initConfig(): void {
 
-    },
-    onCancel: (data, actions) => {
-        console.log('OnCancel', data, actions);
-    },
-    onError: err => {
-        console.log('OnError', err);
-    },
-    onClick: (data, actions) => {
-        console.log('onClick', data, actions);
-    },
-};
+        const price = this.price;
+        const currency = this.currency;
+
+        this.payPalConfig = {
+            currency: this.currency,
+            clientId: this.id_client,
+            // tslint:disable-next-line: no-angle-bracket-type-assertion
+            createOrderOnClient: (data) => < ICreateOrderRequest > {
+                intent: 'CAPTURE',
+                purchase_units: [{
+                    amount: {
+                        currency_code: currency,
+                        value: price,
+                        breakdown: {
+                            item_total: {
+                                currency_code: currency,
+                                value: price
+                            },
+                        }
+                    },
+                    items: [{
+                        name: 'PAIEMENT JSTORE SHOP',
+                        quantity: '1',
+                        category: 'PHYSICAL_GOODS',
+                        unit_amount: {
+                            currency_code: currency,
+                            value: price,
+                        },
+                    }]
+                }]
+            },
+            advanced: {
+                commit: 'true'
+            },
+            style: {
+                label: 'paypal',
+                layout: 'vertical'
+            },
+            onApprove: (data, actions) => {
+                console.log('onApprove - transaction was approved, but not authorized', data, actions);
+                actions.order.get().then(details => {
+                    console.log('onApprove - you can get full order details inside onApprove: ', details);
+                });
+            },
+            onClientAuthorization: (data) => {
+                console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+
+            },
+            onCancel: (data, actions) => {
+                console.log('OnCancel', data, actions);
+            },
+            onError: err => {
+                console.log('OnError', err);
+            },
+            onClick: (data, actions) => {
+                console.log('onClick', data, actions);
+            },
+        };
